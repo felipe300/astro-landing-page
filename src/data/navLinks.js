6a5e334808b194efc54bddcdd288f0daaeee1285
Felipe300 <caracoloro@gmail.com>
2022-03-14 15:41:29 -0300

    feat: Add navbar, footer and basic layout

---
+++ src/data/navLinks.js
@@ -0,0 +1,24 @@
+const navLinks = [
+  {
+    name: 'Home',
+    url: '/',
+    style: 'trasnparent'
+  },
+  {
+    name: 'Pricing',
+    url: '#',
+    style: 'trasnparent'
+  },
+  {
+    name: 'About',
+    url: '#',
+    style: 'trasnparent'
+  },
+  {
+    name: 'Contact Me',
+    url: '#',
+    style: 'primary'
+  }
+]
+
+export default navLinks
